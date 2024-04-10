bb913c99b15af3e20eb9dbfeb01d42f1ce19f426
Azuany Mila Ceron <69259402+azu-any@users.noreply.github.com>
2024-04-10 17:44:35 -0600

    Update index-2b95b968.js

--- assets/index-2b95b968.js
+++ assets/index-2b95b968.js
@@ -276,7 +276,11 @@ Error generating stack: `+i.message+`
       name:"Beach",
       image:"https://i.imgur.com/gcW1jul.jpeg",
       colors:{"--bg-color":"#2E2E2E","--default-color":"#E3C9BC","--accent-color":"#91C6CC","--accent-color2":"#F0F8FA"}
-    }
+    },
+    {
+      name:"Azu",
+      image:"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a2b08080596159.5cecd303f1e28.gif",
+      colors:{"--bg-color":"#24273A","--default-color":"#CAD3F5","--accent-color":"#C6A0F6","--accent-color2":"#8AADF4"}
   ],
   Oi={
     get:()=>{const e=localStorage.getItem("search-settings");
@@ -309,7 +313,7 @@ Error generating stack: `+i.message+`
      },
   Zs="link-groups",
   Wt={getRaw:()=>localStorage.getItem(Zs),get:()=>{const e=localStorage.getItem(Zs);if(e)return Wt.parse(e)},getWithFallback:()=>{try{return Wt.get()??lh}catch{return console.error("Your currently applied links appear to be corrupted."),lh}},set:e=>localStorage.setItem(Zs,JSON.stringify(e)),parse:e=>JSON.parse(e)},
-  Ai={get:()=>{const e=localStorage.getItem("design");if(e)return Dt.parse(e)},getWithFallback:()=>{try{return Ai.get()??$i[5]}catch{return console.error("Your currently applied design appears to be corrupted."),$i[0]}},set:e=>localStorage.setItem("design",JSON.stringify(e))},_2=D.div`
+  Ai={get:()=>{const e=localStorage.getItem("design");if(e)return Dt.parse(e)},getWithFallback:()=>{try{return Ai.get()??$i[13]}catch{return console.error("Your currently applied design appears to be corrupted."),$i[13]}},set:e=>localStorage.setItem("design",JSON.stringify(e))},_2=D.div`
   
   margin-left: 100px;
   display: flex;
@@ -1294,7 +1298,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
   > div {
     border: 2px solid var(--default-color);
   }
-`,hI=({colors:e,setColors:t})=>{const[n,r]=B.useState(Object.keys($i[5].colors)[0]),a=i=>{const o={...e};o[n]=i.hex,t(o)};return k.jsxs(fI,{children:[k.jsx("div",{children:Object.keys(e).map(i=>k.jsx(dI,{active:i===n,onClick:()=>r(i),children:i},i))}),k.jsx(pI,{children:k.jsx(WF,{color:e[n],onChange:i=>i&&a(i)})})]})},gI=D.button`
+`,hI=({colors:e,setColors:t})=>{const[n,r]=B.useState(Object.keys($i[13].colors)[0]),a=i=>{const o={...e};o[n]=i.hex,t(o)};return k.jsxs(fI,{children:[k.jsx("div",{children:Object.keys(e).map(i=>k.jsx(dI,{active:i===n,onClick:()=>r(i),children:i},i))}),k.jsx(pI,{children:k.jsx(WF,{color:e[n],onChange:i=>i&&a(i)})})]})},gI=D.button`
   color: ${({inverted:e})=>e?"var(--bg-color)":"var(--default-color)"};
   background-color: transparent;
   min-width: 50px;
